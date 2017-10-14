f5e64350921c52771588eb9f2344efb1266f53e0
LanaBanana <s.glazyrina@mail.ru>
2017-10-14 13:20:10 +0300

    Add method of add new product(book, film, music)

--- main.rb
+++ main.rb
@@ -11,28 +11,11 @@ require_relative 'product.rb' # подключаем классы
 require_relative 'film.rb'
 require_relative 'music.rb'
 require_relative 'book.rb'
+require 'rexml/document'
 
-products = [] # создаём массив, куда будем загружать товары
+#products = [] # создаём массив, куда будем загружать товары
 
-book1 = Book.new(150, 0) # создаём объект класса "Book"
-book1.update(:title => "Алхимик", :name_author => "Пауло Коэльо") # присваиваем объекту название книги и имя автора
-products << book1 # кладём объект в массив товаров
-
-film1 = Film.new(200, 5) # далее наполняем наш массив товарами
-film1.update(:title => "Убить Билла", :director_name => "Тарантино", :year => 2003)
-products << film1
-
-music = Music.new(100, 200)
-music.update(:album_name => 'Кислород', :artist_name => 'Артем Пивоваров', :genre => 'Pop')
-products << music
-
-book2 = Book.new(300, 15)
-book2.update(:title => "Путешествие в Икстлан", :name_author => "Карлос Кастанеда")
-products << book2
-
-film2 = Film.new(250, 27)
-film2.update(:title => "Титаник", :director_name => "Джеймс Кэмерон", :year => 1997)
-products << film2
+products = Product.read_from_xml('/products.xml')
 
 user_buy = 0 # Изначально общая стоимость товаров в корзине покупателя "ноль"
 
@@ -44,7 +27,7 @@ puts "Какой товар желаете приобрести?"
 
 choice = STDIN.gets.chomp # записываем в переменную choice выбор покупаятеля
 
- while (choice != "x" && choice.to_i == 0) ||  !(choice.to_i =~ /^\d$/) && !(choice.to_i <= products.size) # проверяем, что пользователь ввёл цифру, не превышающую кол-во товаров в массиве
+ while (choice != "x" && choice != "d" && choice.to_i == 0) ||  !(choice.to_i =~ /^\d$/) && !(choice.to_i <= products.size) # проверяем, что пользователь ввёл цифру, не превышающую кол-во товаров в массиве
 
   Product.show_all(products)
   puts "Выберите товар или нажмите x"
@@ -52,25 +35,56 @@ choice = STDIN.gets.chomp # записываем в переменную choice 
 
  end
 
-
-
 break if choice == "x" # прекращаем цикл, если пользователь ввёл "x"
 
-if products[choice.to_i - 1].amount > 0 # условие: если кол-во товара на складе больше нуля,
+if choice == "d"
+  puts "Какой товар Вы хотите добавить?\n1. Book\n2. Film\n3. Music"
+  user_add = STDIN.gets.to_i
 
-user_buy += products[choice.to_i - 1].price # то цену товара записываем в корзину покупателя
+    puts "Укажите стоимость продукта в рублях"
+    user_cost = STDIN.gets.chomp
+    puts "Укажите, сколько ед. продукта осталось на складке"
+    user_count = STDIN.gets.chomp
+  if user_add == 1
+    puts "Укажите автора книги"
+    user_author = STDIN.gets.chomp
+    puts "Укажите название книги"
+    user_title = STDIN.gets.chomp
+    Product.add_book(user_cost, user_count, user_title, user_author, 'products.xml')
+  elsif user_add == 2
+    puts "Укажите название фильма"
+    user_title = STDIN.gets.chomp
+    puts "Укажите режиссера"
+    user_director = STDIN.gets.chomp
+    puts "Укажите год создания фильма"
+    user_year = STDIN.gets.chomp
+    Product.add_film(user_cost, user_count, user_title, user_director, user_year,'products.xml')
+  elsif user_add == 3
+    puts "Укажите исполнителя"
+    user_artist = STDIN.gets.chomp
+    puts "Укажите название песни"
+    user_album_name = STDIN.gets.chomp
+    puts "Укажите жанр"
+    user_genre = STDIN.gets.chomp
+    Product.add_music(user_cost, user_count, user_album_name, user_artist, user_genre,'products.xml' )
+  end
+  puts "Товар успешно добавлен"
+elsif
+
+  if products[choice.to_i - 1].amount > 0 # условие: если кол-во товара на складе больше нуля,
+
+     user_buy += products[choice.to_i - 1].price # то цену товара записываем в корзину покупателя
+
+  end
+
+  puts products[choice.to_i - 1].buy # отображаем покупателю информацию о купленном им товаре
 
 end
-
-puts products[choice.to_i - 1].buy # отображаем покупателю информацию о купленном им товаре
-
 end
-
 if user_buy == 0 # если покупатель покинул магазин и не приобрел ни одного товара, то выводим сообщение
   puts "У вас 0 покупок! Всего доброго. Может быть, в следующий раз Вам удастся что-нибудь подобрать"
 else
   puts "Спасибо за покупки! С вас #{user_buy} руб." # если покупатель выбрал товары, то выводим сумму его покупок
 end
-
 
 
--- product.rb
+++ product.rb
@@ -32,6 +32,7 @@ class Product
     end
 
     puts "x. Покинуть магазин"
+    puts "d. Добавить товар"
   end
 
 
@@ -46,6 +47,93 @@ class Product
       puts "*   *   *"
     end
 
+  end
+
+  def self.read_from_xml(file)
+    file_path = File.dirname(__FILE__) + file
+    abort "Файл не найден" unless File.exist?(file_path)
+
+    file_name = File.new(file_path,"r:utf-8")
+    doc = REXML::Document.new(file_name)
+    file_name.close
+
+    result = []
+    product = nil
+    doc.elements.each('products/product') do |item|
+      price = item.attributes['price'].to_i
+      amount = item.attributes['amount'].to_i
+
+      item.each_element('book') do |book_item|
+      product = Book.new(price, amount)
+      product.update(:title => book_item.attributes['title'], :name_author => book_item.attributes['author'])
+      end
+
+      item.each_element('film') do |film_item|
+        product = Film.new(price, amount)
+        product.update(:title => film_item.attributes['title'], :director_name => film_item.attributes['director'], :year => film_item.attributes['year'])
+      end
+
+      item.each_element('music') do |music_item|
+        product = Music.new(price, amount)
+        product.update(:album_name => music_item.attributes['album_name'], :artist_name => music_item.attributes['artist'], :genre => music_item.attributes['genre'])
+      end
+      result.push(product)
+    end
+      return result
+  end
+
+  def self.add_book(user_cost, user_count, user_title, user_author,file)
+    file_path = File.dirname(__FILE__) + "/" + file
+    abort "Файл не найден" unless File.exist?(file_path)
+
+    file_name = File.new(file_path,"r:utf-8")
+    doc = REXML::Document.new(file_name)
+    file_name.close
+
+    product = doc.root.add_element "product", {'price' => user_cost, 'amount' => user_count}
+
+    if !product.has_elements?
+    product.add_element "book", {'title' => user_title, 'author' => user_author}
+    end
+    file_name = File.new(file_path,"w:utf-8")
+    doc.write(file_name, 2)
+    file_name.close
+  end
+
+  def self.add_film(user_cost, user_count, user_title, user_director, user_year, file)
+    file_path = File.dirname(__FILE__) + "/" + file
+    abort "Файл не найден" unless File.exist?(file_path)
+
+    file_name = File.new(file_path,"r:utf-8")
+    doc = REXML::Document.new(file_name)
+    file_name.close
+
+    product = doc.root.add_element "product", {'price' => user_cost, 'amount' => user_count}
+
+    if !product.has_elements?
+      product.add_element "film", {'title' => user_title, 'director' => user_director, 'year' => user_year}
+    end
+    file_name = File.new(file_path,"w:utf-8")
+    doc.write(file_name, 2)
+    file_name.close
+  end
+
+  def self.add_music(user_cost, user_count, user_album_name, user_artist, user_genre, file)
+    file_path = File.dirname(__FILE__) + "/" + file
+    abort "Файл не найден" unless File.exist?(file_path)
+
+    file_name = File.new(file_path,"r:utf-8")
+    doc = REXML::Document.new(file_name)
+    file_name.close
+
+    product = doc.root.add_element "product", {'price' => user_cost, 'amount' => user_count}
+
+    if !product.has_elements?
+      product.add_element "music", {'album_name' => user_album_name, 'artist' => user_artist, 'genre' => user_genre}
+    end
+    file_name = File.new(file_path,"w:utf-8")
+    doc.write(file_name, 2)
+    file_name.close
   end
 
 end
