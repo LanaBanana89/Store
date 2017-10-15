1d50c9c49327e164da0a73ab3cc54fe9515aab12
LanaBanana89 <32016884+LanaBanana89@users.noreply.github.com>
2017-10-15 17:53:58 +0300

    Update product.rb

--- product.rb
+++ product.rb
@@ -82,11 +82,11 @@ class Product
       return result
   end
 
-  def to_xml
+  def to_xml # абстрактный метод, который реализован у дочерних классов
 
   end
 
-  def self.save_to_xml(product, product_xml)
+  def self.save_to_xml(product, product_xml) # метод добавления продукта в xml-файл
     file_path = File.dirname(__FILE__) + "/products.xml"
     abort "Файл не найден" unless File.exist?(file_path)
 
@@ -96,13 +96,6 @@ class Product
 
     el_product = doc.root.add_element "product", {'price' => product.price ,'amount' => product.amount}
     el_product.elements << product_xml
-    #if product.class == 'Book'
-    #el_product << product.to_xml(user_title, user_author)
-    #elsif product.class == 'Film'
-    #el_product << product.to_xml(user_title, user_director, user_year)
-    #end
-
-
 
     file = File.new(file_path,"w:utf-8")
     doc.write(file, 2)
